aafc56b101d5b4967e4377019de1d3463e3abc30
FLeijten <floris.leijten@vivideconomics.com>
2022-09-29 12:50:34 -0400

    create new patch file

---
+++ scripts/start/iluc_runs/create_new_patch_file.R
@@ -0,0 +1,21 @@
+setwd("~/GitHub/magpie/patch_inputdata")
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_shock.tgz") 
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_baseline.tgz") 
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_baseline_no_proc_shares.tgz") 
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_shock_27_09.tgz") 
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_baseline_27_09.tgz") 
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_shock_add_50_perc_27_09.tgz") 
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_baseline_27_09_debug.tgz") 
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_shock_27_09_linear.tgz") 
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_baseline_27_09_debug_linear.tgz") 
+
+#gms::tardir(dir="new_data_patch", tarfile="patch_iluc_LCFS_shock_subtract_50_perc_27_09.tgz") 
+
+setwd("C:/Users/Floris Leijten/OneDrive - McKinsey & Company/Documents/GitHub/magpie")
